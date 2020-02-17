c5ccd5878257e66a85c1eccaf2d59568c0c8e8ef
sonlamthai <sonlamthai.vn@gmail.com>
2020-02-17 21:45:19 +0700

    update input phone number

--- validate/user.validate.js
+++ validate/user.validate.js
@@ -2,23 +2,21 @@ module.exports.createPost = (req, res, next) => {
 
 
     let errors = [];
-    let digits = '0123456789';
 
-    if (digits.indexOf(req.body.phone.charAt(0)) === -1) {
-        errors.push('So dien thoai khong hop le')
+    for (i = 0; i < req.body.phone.length; i++) {
+        console.log(req.body.phone.charCodeAt(i));
+        if (!(req.body.phone.charCodeAt(i) >= 48 && req.body.phone.charCodeAt(i) <= 57)) {
+            errors.push('So dien thoai khong hop le');
+            break;
+        }
     }
 
-    else {
+    if (!req.body.name) {
+        errors.push('Chua nhap ten')
+    }
 
-        if (!req.body.name) {
-            errors.push('Chua nhap ten')
-        }
-
-        if (!req.body.phone) {
-            errors.push('Chua nhap phone')
-        }
-
-
+    if (!req.body.phone) {
+        errors.push('Chua nhap phone')
     }
 
     if (errors.length) {
