7c12f4de8c40e9fc57b15115fd31338a5e13ff1c
Lam Thai Son <sonlamthai.vn@gmail.com>
2020-06-21 00:01:04 +0700

    test branch 2

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+
+  console.log("test branch 2");
 });
 
 module.exports = app;
